fa3fc2275073aca2319a86c0159ae6a9749c75dc
Juan Garcia <juagarca@gmail.com>
2020-11-12 14:00:07 +0000

    added doses#new & create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/cocktails', to: 'cocktails#index'
-  get '/cocktails/new', to: 'cocktails#new'
-  get '/cocktails/:id', to: 'cocktails#show'
-  post '/cocktails', to: 'cocktails#create'
+  get 'cocktails', to: 'cocktails#index'
+  get 'cocktails/new', to: 'cocktails#new'
+  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  post 'cocktails', to: 'cocktails#create'
+
+  get 'cocktails/:id/doses/new', to: 'doses#new', as: :doses
+  post 'cocktails/:id/doses', to: 'doses#create', as: :cocktail_doses
 end
