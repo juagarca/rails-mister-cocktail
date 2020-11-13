fbfa0bb633ed7133ea88daa21ce2272bda2367f0
Juan Garcia <juagarca@gmail.com>
2020-11-13 13:27:50 +0000

    added photo functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,9 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import 'bootstrap';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
