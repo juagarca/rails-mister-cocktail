f9457df460de9c1efc6d7f3124e7a87a66ef9847
Juan Garcia <juagarca@gmail.com>
2020-11-13 16:20:44 +0000

    navbar transparency

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,8 +1,10 @@
 const initUpdateNavbarOnScroll = () => {
+
   const navbar = document.querySelector('.navbar');
+
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 1 ) {
         navbar.classList.add('navbar-white');
       } else {
         navbar.classList.remove('navbar-white');
