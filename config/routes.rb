60a9b5789447b2bcc595992eda0f8fcabbde726c
Juan Garcia <juagarca@gmail.com>
2020-11-13 08:55:49 +0000

    created home page and added banners

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
+  root to: 'cocktails#home'
   resources :cocktails, only: %i[index new show create] do
     resources :doses, only: %i[new create]
   end
